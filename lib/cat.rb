beb46302c43ccce6b82394e9e1fa65cafd5c7f29
syd333 <sydmiranda@icloud.com>
2021-01-19 22:00:18 -0500

    done

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
 class Cat
   # code goes here
+
+  @@all = []
+
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  def self.all
+    @@all
+  end
+
+def initialize(name, owner)
+  @name = name
+  @owner = owner
+  @mood = "nervous"
+  @@all << self 
+end
+
+
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
   # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self 
+  end
+
+
 end
